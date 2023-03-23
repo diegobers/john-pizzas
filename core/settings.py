9942013fa1c10eaeb80292c75c79338a2fed8f80
Diego Bernardi <diegobernardisza@gmail.com>
2023-03-23 17:28:59 -0300

    set BR internationalization settings

--- core/settings.py
+++ core/settings.py
@@ -76,8 +76,8 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 # Internationalization
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'pt-BR'
+TIME_ZONE = 'America/Sao_Paulo'
 USE_I18N = True
 USE_TZ = True
 
