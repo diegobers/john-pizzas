d36e4216aaccff1a058fdba5f8b307d2fb2e605f
diegobers <diegobernardisza@gmail.com>
2023-06-23 22:13:58 -0300

    update settings

--- core/settings.py
+++ core/settings.py
@@ -73,7 +73,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ["PGDATABASE"],
         'USER': os.environ["PGUSER"],
         'PASSWORD': os.environ["PGPASSWORD"],
