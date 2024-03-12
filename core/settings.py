7c8e3b59cd36d5787e3e968c438c9ea2ac45f3dc
Diego Bernardi <diegobernardisza@gmail.com>
2024-03-12 11:13:47 -0300

    add django-environ for environment variables

--- core/settings.py
+++ core/settings.py
@@ -1,73 +1,23 @@
 import os
-import dj_database_url
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+import environ
 
-SECRET_KEY = '***'
 
-DEBUG = True
+BASE_DIR = environ.Path(__file__) - 3
+env = environ.Env()
+env.read_env(BASE_DIR('.env'))
 
-ALLOWED_HOSTS = ['*']
-
-SITE_ID = 1
+DEBUG = env('DEBUG')
+SECRET_KEY = env('SECRET_KEY')
 
 AUTH_USER_MODEL = 'accounts.JohnPizzaAbstractUserModel'
 
-LOGIN_URL = 'login'
-LOGIN_REDIRECT_URL = 'dashboard'
-LOGOUT_REDIRECT_URL = 'index'
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-#EMAIL_HOST = 'smtp.gmail.com'
-#EMAIL_USE_TLS = False
-#EMAIL_PORT = 587
-#EMAIL_HOST_USER = 'diegobernardisza@gmail.com'
-#EMAIL_HOST_PASSWORD = '!Ogeid06$%1994#'
-#EMAIL_USE_SSL = True
-
-ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
-ACCOUNT_CONFIRM_EMAIL_ON_GET = True
-#ACCOUNT_EMAIL_CONFIRMATION_ANONYMOUS_REDIRECT_URL = settings.LOGIN_URL
-#ACCOUNT_EMAIL_CONFIRMATION_AUTHENTICATED_REDIRECT_URL = None
-ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS = 1
-#ACCOUNT_EMAIL_CONFIRMATION_HMAC = True
-ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_EMAIL_VERIFICATION = 'optional'
-#ACCOUNT_EMAIL_CONFIRMATION_COOLDOWN = 180
-ACCOUNT_MAX_EMAIL_ADDRESSES = 2
-ACCOUNT_LOGIN_ATTEMPTS_LIMIT = 3
-ACCOUNT_LOGIN_ATTEMPTS_TIMEOUT = 1
-#ACCOUNT_LOGIN_ON_EMAIL_CONFIRMATION = True
-ACCOUNT_LOGOUT_ON_GET = True
-#ACCOUNT_LOGOUT_ON_PASSWORD_CHANGE = False
-#ACCOUNT_LOGIN_ON_PASSWORD_RESET = False
-ACCOUNT_PRESERVE_USERNAME_CASING = True
-ACCOUNT_PREVENT_ENUMERATION = True
-#ACCOUNT_RATE_LIMITS = {}
-ACCOUNT_SESSION_REMEMBER = None
-ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = False
-ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = False
-ACCOUNT_UNIQUE_EMAIL = True
-ACCOUNT_USER_MODEL_EMAIL_FIELD = 'email'
-ACCOUNT_USER_MODEL_USERNAME_FIELD = 'username'
-ACCOUNT_USERNAME_MIN_LENGTH = 1
-ACCOUNT_USERNAME_REQUIRED = True
-
-SOCIALACCOUNT_AUTO_SIGNUP = True
-#SOCIALACCOUNT_EMAIL_VERIFICATION = settings.ACCOUNT_EMAIL_VERIFICATION
-#SOCIALACCOUNT_EMAIL_REQUIRED = settings.ACCOUNT_EMAIL_REQUIRED
-SOCIALACCOUNT_LOGIN_ON_GET = True
-SOCIALACCOUNT_QUERY_EMAIL = True
-
-CSRF_ALLOWED_ORIGINS = ['https://john-pizzas-production.up.railway.app']
-
-CSRF_TRUSTED_ORIGINS = ['https://john-pizzas-production.up.railway.app']
-
-CORS_ORIGINS_WHITELIST = ['https://john-pizzas-production.up.railway.app']
+ALLOWED_HOSTS = env.list('HOSTS')
+SITE_ID = 1
 
 
-# Application definition
+# App's
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -78,14 +28,18 @@ INSTALLED_APPS = [
     'django.contrib.sites',
 
     # my app's
-    'orders',
     'accounts',
     'pages',
+    'catalogue',
+    #'orders',
+    #'checkout',
+
     # third lib
     'allauth',
     'allauth.account',
-    'allauth.socialaccount',
-    'allauth.socialaccount.providers.google',
+    #'allauth.socialaccount',
+    #'allauth.socialaccount.providers.google',
+
 ]
 
 MIDDLEWARE = [
@@ -96,7 +50,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
@@ -104,7 +57,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "templates")],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -112,7 +65,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.media',
             ],
         },
     },
@@ -121,24 +73,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'core.wsgi.application'
 
 # Database
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+
+DATABASES = { 
+    'default': env.db(),
 }
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'postgres',
-#        'USER': 'postgres',
-#        'PASSWORD': 'postgres',
-#        'HOST': 'localhost',
-#        'PORT': 5432,
-#    }
-#}
-
+# Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -154,45 +94,21 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTHENTICATION_BACKENDS = [
-    'django.contrib.auth.backends.ModelBackend',
-    'allauth.account.auth_backends.AuthenticationBackend',
-]
-
 # Internationalization
-LANGUAGE_CODE = 'pt-BR'
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'pt-Br'
+TIME_ZONE = 'America/Sao_Paulo'
 USE_I18N = True
 USE_TZ = True
 USE_THOUSAND_SEPARATOR = True
 
-# Static files (CSS, JavaScript, Images)
+# Static files
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [ BASE_DIR / "static", ]
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
+# Base url to serve media files
+MEDIA_URL = '/media/'
+# Path where media is stored
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# AWS S3 Bucket with Boto3 and django-storages
-AWS_ACCESS_KEY_ID = 'AKIA3COXWJFX6YQFBPRS'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'aws-s3djangoapps'
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-
-# Provider specific settings
-SOCIALACCOUNT_PROVIDERS = {
-    'google': {
-        'APP': {
-            'client_id': '796086142721-0a5gl9nfmt6umkvtprgus3hneii3n3cl.apps.googleusercontent.com',
-            'secret': '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',
-            'key': ''
-        }
-    }
-}
