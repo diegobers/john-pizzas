5247a7b053276e9aaed5da92bb1a1029a7d53cf2
Diego Bernardi <diegobernardisza@gmail.com>
2024-05-15 00:48:07 -0300

    update index template img field

--- store/models.py
+++ store/models.py
@@ -5,6 +5,7 @@ from django.conf import settings
 class Pizza(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=5, decimal_places=2)
+    photo_main = models.ImageField(upload_to='product/', default='/product/pizza.jpeg')
 
     def __str__(self):
         return self.name
