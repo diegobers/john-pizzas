9db3a3ac7836a52f474f6529f779212988408198
Diego Bernardi <diegobernardisza@gmail.com>
2024-05-30 04:05:31 -0300

    update templates layouts

--- store/forms.py
+++ store/forms.py
@@ -11,15 +11,15 @@ class PizzaForm(forms.ModelForm):
 class CheckoutForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ['shipping_address', 'payment_method', 'observation', 'is_shipping']
+        fields = ['is_shipping', 'shipping_address', 'payment_method', 'observation']
 
     def __init__(self, *args, **kwargs):
         user = kwargs.pop('user', None)
         super().__init__(*args, **kwargs)
+        self.fields['is_shipping'].widget.attrs.update({'class': 'form-check-input'})
         self.fields['shipping_address'].widget.attrs.update({'class': 'form-control'})
         self.fields['payment_method'].widget.attrs.update({'class': 'form-control'})
         self.fields['observation'].widget.attrs.update({'class': 'form-control'})
-        self.fields['is_shipping'].widget.attrs.update({'class': 'form-check-input'})
         
         if user:
             self.fields['shipping_address'].initial = user.address if user else ''
